78f873033749793cfa309e8db1e2e14b0faa9819
Kieren Davies <kieren@kdavi.es>
2022-12-05 20:20:55 +0100

    Solve 4b

---
+++ src/bin/day04b.rs
@@ -0,0 +1,80 @@
+use core::fmt;
+use std::error;
+use std::io;
+use std::str;
+
+#[derive(Debug)]
+struct ParseError {
+    input: String,
+}
+
+impl ParseError {
+    fn err<T>(input: &str) -> Result<T, ParseError> {
+        Err(ParseError {
+            input: input.to_owned(),
+        })
+    }
+}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "ParseError({:?})", self.input)
+    }
+}
+
+impl error::Error for ParseError {}
+
+#[derive(Debug)]
+struct Assignment {
+    start: i32,
+    end: i32,
+}
+
+impl Assignment {
+    fn contains_section(&self, section: i32) -> bool {
+        self.start <= section && section <= self.end
+    }
+}
+
+impl str::FromStr for Assignment {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<_> = s.split('-').collect();
+        match parts.as_slice() {
+            &[start_str, end_str] => {
+                let start = start_str.parse().or(ParseError::err(s))?;
+                let end = end_str.parse().or(ParseError::err(s))?;
+                Ok(Assignment { start, end })
+            }
+            _ => ParseError::err(s),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Pair(Assignment, Assignment);
+
+impl str::FromStr for Pair {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<_> = s.split(',').collect();
+        match parts.as_slice() {
+            &[l_str, r_str] => {
+                let l = l_str.parse().or(ParseError::err(s))?;
+                let r = r_str.parse().or(ParseError::err(s))?;
+                Ok(Pair(l, r))
+            }
+            _ => ParseError::err(s),
+        }
+    }
+}
+
+fn main() {
+    let input = io::stdin().lines().map(Result::unwrap);
+    let pairs = input.map(|line| line.parse::<Pair>().unwrap());
+    let overlapping =
+        pairs.filter(|Pair(l, r)| l.contains_section(r.start) || r.contains_section(l.start));
+    println!("{}", overlapping.count());
+}
