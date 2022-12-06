248c2bff6ced541e6fe40bd94438a5d9445eee73
Kieren Davies <kieren@kdavi.es>
2022-12-06 12:40:34 +0100

    Solve 6a

---
+++ src/bin/day06a.rs
@@ -0,0 +1,22 @@
+use std::io;
+
+const MARKER_SIZE: usize = 4;
+
+fn main() {
+    let mut input: String = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let data: Vec<char> = input.trim().chars().collect();
+
+    let (offset, _) = data
+        .windows(MARKER_SIZE)
+        .enumerate()
+        .find(|(_, window)| {
+            let mut distinct = window.to_vec();
+            distinct.sort();
+            distinct.dedup();
+            distinct.len() == MARKER_SIZE
+        })
+        .unwrap();
+
+    println!("{}", offset + MARKER_SIZE);
+}
