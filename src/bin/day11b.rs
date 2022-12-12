1faea6939de64b38d79390fd25d2d79db96575e2
Kieren Davies <kieren@kdavi.es>
2022-12-12 10:56:45 +0100

    Solve 11b

---
+++ src/bin/day11b.rs
@@ -0,0 +1,109 @@
+use std::vec;
+
+const ROUNDS: u32 = 10_000;
+
+struct Monkey<'a> {
+    items: Vec<u64>,
+    op: &'a dyn Fn(u64) -> u64,
+    test_div: u64,
+    if_true: usize,
+    if_false: usize,
+    inspections: u64,
+}
+
+fn main() {
+    // Ugh, I don't want to write a parser for this.
+    let monkeys: &mut [Monkey] = &mut [
+        Monkey {
+            items: vec![54, 53],
+            op: &|w| w * 3,
+            test_div: 2,
+            if_true: 2,
+            if_false: 6,
+            inspections: 0,
+        },
+        Monkey {
+            items: vec![95, 88, 75, 81, 91, 67, 65, 84],
+            op: &|w| w * 11,
+            test_div: 7,
+            if_true: 3,
+            if_false: 4,
+            inspections: 0,
+        },
+        Monkey {
+            items: vec![76, 81, 50, 93, 96, 81, 83],
+            op: &|w| w + 6,
+            test_div: 3,
+            if_true: 5,
+            if_false: 1,
+            inspections: 0,
+        },
+        Monkey {
+            items: vec![83, 85, 85, 63],
+            op: &|w| w + 4,
+            test_div: 11,
+            if_true: 7,
+            if_false: 4,
+            inspections: 0,
+        },
+        Monkey {
+            items: vec![85, 52, 64],
+            op: &|w| w + 8,
+            test_div: 17,
+            if_true: 0,
+            if_false: 7,
+            inspections: 0,
+        },
+        Monkey {
+            items: vec![57],
+            op: &|w| w + 2,
+            test_div: 5,
+            if_true: 1,
+            if_false: 3,
+            inspections: 0,
+        },
+        Monkey {
+            items: vec![60, 95, 76, 66, 91],
+            op: &|w| w * w,
+            test_div: 13,
+            if_true: 2,
+            if_false: 5,
+            inspections: 0,
+        },
+        Monkey {
+            items: vec![65, 84, 76, 72, 79, 65],
+            op: &|w| w + 5,
+            test_div: 19,
+            if_true: 6,
+            if_false: 0,
+            inspections: 0,
+        },
+    ];
+
+    let mod_base: u64 = monkeys.iter().map(|m| m.test_div).product();
+
+    for _round in 0..ROUNDS {
+        for turn in 0..monkeys.len() {
+            let items = monkeys[turn].items.clone();
+            for item in items {
+                let item = (monkeys[turn].op)(item) % mod_base;
+                let throw_to = if item % monkeys[turn].test_div == 0 {
+                    monkeys[turn].if_true
+                } else {
+                    monkeys[turn].if_false
+                };
+                monkeys[throw_to].items.push(item);
+            }
+            monkeys[turn].inspections += monkeys[turn].items.len() as u64;
+            monkeys[turn].items.clear();
+        }
+    }
+
+    let mut inspections: Vec<u64> = monkeys.iter().map(|m| m.inspections).collect();
+    inspections.sort_by(|a, b| b.cmp(a));
+    let monkey_business = match inspections.as_slice() {
+        [a, b, ..] => a * b,
+        _ => unreachable!(),
+    };
+    println!("{}", monkey_business);
+}
